b4183b433e2602dca1cbdb4554791815c5d4c7b1
Ben Aldrich <benaldrich2000@gmail.com>
2020-11-04 11:59:35 -0800

    Images and Text
    Added Images and text from the website

--- FinalProject/src/FinalProject.java
+++ FinalProject/src/FinalProject.java
@@ -1,7 +1,28 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class FinalProject extends Application{
+	
+	/*Final Project layout for CS2450 Class
+	 * Rewrite of the the website
+	 * http://www.gatesnfences.com/
+	 * to make improvements to appearance
+	 * */
+	
+	//window size
+	static final double WIDTH = 1000.0, HEIGHT = 1500.0;
+	
+	static final String aboutSec1 = "All of our Aluminum or Wrought Iron Gates, or Fences are designed and manufactured to withstand a range of outdoor conditions. Our\r\n" + 
+			"commitment to our customers and dedication to produce quality gates has earned us thousands of satisfied customers.";
+	static final String aboutSec2 = "Although we offer a wide selection or Ornamental Designs or Decorative Designs, we can design and manufacture any style in aluminum or\r\n" + 
+			"wrought iron metals. L. A. Ornamental & Rack Corp also offers Fences, Garden or Walk Thru Gates to match your driveway  gates. With over\r\n" + 
+			"thirty five years of experience in manufacturing and designing elegant, custom, or exotic Aluminum Driveway Gates and Fences, our past and\r\n" + 
+			"future customers can have peace of mind that they are receiving quality workmanship.  We are a Fence Company that gives our customers\r\n" + 
+			"110% of dedication to manufacture quality driveway gates and fences.\r\n" + 
+			"For a quote please send an e-mail to LAOrnamental@aol.com";
+	static final String aboutSec3 = "";
 
 	public static void main(String[] args) {
 		launch(args);
@@ -11,6 +32,13 @@ public class FinalProject extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
+		
+		VBox vbox = new VBox();
+		
+		// Create a Scene and display it.
+		Scene scene = new Scene(vbox, WIDTH, HEIGHT);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 		
 	}
 
